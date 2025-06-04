18bfc025ffe5148912d43bd5618f26f65a9ef589
Eugen888-oss <eugen.risling@stud.thga.de>
2025-06-04 20:51:42 +0200

    Created sorce file in C

---
+++ solutions/sample.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	    printf("Hello, PP7!\n");
+	        return 0;
+}#include <stdio.h>
+
+int main(void) {
+	    printf("Hello, PP7!\n");
+	        return 0;
+}
